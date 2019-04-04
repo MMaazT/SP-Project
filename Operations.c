76b9e9fe41894c9627a180ee00bb0cc812dc336e
MMaazT <mmaaztariq1997@hotmail.com>
2019-04-04 08:43:09 +0500

    Made a few changes

--- Operations.c
+++ Operations.c
@@ -5,11 +5,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc , char *argv){
+int main(int argc , char *argv[]){
 
 char buff1[500];
 int w1=write(STDOUT_FILENO, "Enter the command:\n", 20);
+if (w1==-1){
+    perror("Write1: ");
+}
 int r1=read(STDIN_FILENO,buff1, sizeof(buff1));
+if (r1==-1){
+    perror("Read1: ");
+}
 buff1[r1-1]='\0';
 char *token= strtok(buff1, " ");
 if(strcmp(token, "add")==0){
@@ -22,20 +28,34 @@ if(strcmp(token, "add")==0){
     char sumbuff[10];
     int s =sprintf(sumbuff, "%d\n", sum);
     if(s==-1) perror("sprintf:");
-    write(STDOUT_FILENO, sumbuff, s);
+        write(STDOUT_FILENO, sumbuff, s);
 }
 else if(strcmp(token, "sub")==0){
-    int diff;
     token= strtok(NULL, " ");
-    diff=atoi(token);
+    int sub;
+    sub=atoi(token);
     while(token!=NULL){
-        diff-=atoi(token);
-        token=strtok(NULL, " ");
+        token= strtok(NULL, " ");
+        sub= sub - atoi(token);
     }
+    //printf("%i\n", sub);
     char diffbuff[10];
-    int s =sprintf(diffbuff, "%d\n", diff);
+    int s =sprintf(diffbuff, "%d\n", sub);
     if(s==-1) perror("sprintf:");
     write(STDOUT_FILENO, diffbuff, s);
+}
+else if(strcmp(token, "mul")==0){
+    int mul=0;
+    token= strtok(NULL, " "); //store the first number in token
+    int tok2=1;
+    mul=atoi(token); //assign it to mult
+    printf("%d %d", mul, tok2);
+    while(token!=NULL){
+        token= strtok(NULL, " "); //store the current number
+        mul= mul * (tok2); //the current number gets multiplied by the next number in the string
+        tok2=atoi(token); // stores the next number of the string.
+    }
+        printf("%i\n", mul);
 }
 }
 
